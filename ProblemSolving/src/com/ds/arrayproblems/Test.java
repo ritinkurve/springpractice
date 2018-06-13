7cc8903ea29c0ea99b7a9656b149ff45fa5ce371
ritinkurve <ritinkurve@gmail.com>
2018-06-13 23:11:12 +0530

    HashSet Demo

--- ProblemSolving/src/com/ds/arrayproblems/Test.java
+++ ProblemSolving/src/com/ds/arrayproblems/Test.java
@@ -1,5 +1,19 @@
 package com.ds.arrayproblems;
 
-public class Test {
+import java.util.HashSet;
+import java.util.Iterator;
 
+public class Test {
+	public static void main(String[] args) {
+
+		int arr[] = { 5, 22, 5, 1, 22, 34, 16, 16, 36, 2 };
+		HashSet<Integer> hasSet = new HashSet<>();
+		for (int i = 0; i < arr.length; i++) {
+			hasSet.add(arr[i]);
+		}
+		Iterator<Integer> itr=hasSet.iterator();
+		while(itr.hasNext()) {
+			System.out.print(itr.next()+ " ");
+		}
+	}
 }
